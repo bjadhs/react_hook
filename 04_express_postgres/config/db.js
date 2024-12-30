dfc6eba65543423da82ee210ff150fc0184c632b
Bijaya Adhikari <bjadhs@gmail.com>
2024-12-30 22:23:22 +1100

    shadcn init with button

---
+++ 04_express_postgres/config/db.js
@@ -0,0 +1,14 @@
+import pg from 'pg';
+import {Pool} from 'pg';
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+})
+
+pool.on('connect',()=>{
+    console.log('connected to the db');
+})
